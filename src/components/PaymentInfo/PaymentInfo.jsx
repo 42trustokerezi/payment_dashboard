6cbd3d09d7961539602b83f7bd4439e51c1b786e
Trust Okerezi <trustokerezi42@gmail.com>
2022-02-03 08:49:20 +0100

    final code

--- src/components/PaymentInfo/PaymentInfo.jsx
+++ src/components/PaymentInfo/PaymentInfo.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
-import { Container, Mop, Title, Text, SubText, PaymentPortals, List_item, Card, CardDetails, Img, Image } from "./Style"
+import { 
+    Container, 
+    Mop, 
+    Title, 
+    Text, 
+    SubText, 
+    PaymentPortals, 
+    List_item, 
+    Card, 
+    CardDetails, 
+    Img, 
+    Image ,
+    Form, 
+    FormGroup, 
+    Label, 
+    FormInput, 
+    Radio, 
+    RadioInput, 
+    RadText,
+    Button,
+    Line,
+    Div,
+    PromoCode,
+    PromoText,
+    Code,
+    SubTotal,
+    SubTText,
+    EstText,
+    EstTax,
+    Payment,
+    Pay,
+    Total,
+    TText,
+    Value,
+} from "./Style"
 import visa from '../../images/visa.png';
 import discover from '../../images/discover.png';
 import paypal from '../../images/paypal.png';
@@ -31,7 +65,87 @@ function PaymentInfo() {
             <Card>
                 <Image src={card}/>
             </Card>
+
+            <Form>
+                <FormGroup>
+                    <Label for="Credit card number">Credit card number</Label>
+                    <FormInput
+                        type="text"
+                        name="CreditCardNumber"
+                        id="CreditCardNumber"
+                        required
+                        placeholder="4324 5433 9382 1030"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="Expiration Date">Expiration Date</Label>
+                    <FormInput
+                        type="text"
+                        name="ExpirationDate"
+                        id="ExpirationDate"
+                        required
+                        placeholder="03/24"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="Security Code">Security Code</Label>
+                    <FormInput
+                        type="text"
+                        name="SecurityCode"
+                        id="SecurityCode"
+                        required
+                        placeholder="420"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="Postal Code">Postal Code</Label>
+                    <FormInput
+                        type="text"
+                        name="PostalCode"
+                        id="PostalCode"
+                        required
+                        placeholder="10119"
+                    />
+                </FormGroup>
+                <FormGroup className="full">
+                        <Radio for="RadioiD">
+                        <RadioInput
+                            type="radio"
+                            name="radioField"
+                            id="RadioId"
+                            required
+                        />
+                        <RadText>use this card for next time purchases</RadText>
+                        </Radio>
+                </FormGroup>
+                <FormGroup className="full">
+                    <Button>Add card</Button>
+                </FormGroup>
+            </Form>
           </CardDetails>
+          <Line></Line>
+          <Div>
+              <SubTotal>
+                  <SubTText>Subtotal</SubTText>
+                  <Value>2,497.00</Value>
+              </SubTotal>
+              <EstTax>
+                  <EstText>Estimated Tax</EstText>
+                  <Value>119.64</Value>
+              </EstTax>
+              <PromoCode>
+                  <PromoText>Promotional Code:<Code>Z4KXLM9A</Code></PromoText>
+                  <Value>119.64</Value>
+              </PromoCode>
+          </Div>
+          <Line></Line>
+          <Payment>
+              <Pay>Complete payment</Pay>
+              <Total>
+                  <TText>Total:</TText>
+                  <Value>2556.64</Value>
+              </Total>
+          </Payment>
       </Container>
     );
 }
--- src/components/PaymentInfo/Style.js
+++ src/components/PaymentInfo/Style.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+
+`;
 
 export const Mop = styled.div`
     display: flex;
@@ -26,13 +28,17 @@ export const PaymentPortals = styled.ul`
     list-style-type: none;
 `;
 
-export const CardDetails = styled.div``;
+export const CardDetails = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 3rem;
+`;
 
 export const Card = styled.div`
     padding: 0;
-    width: 30rem;
-    height: 20rem;
-    margin: 0 3rem;
+    width: 25rem;
+    height: 16rem;
 `;
 
 export const List_item = styled.li`
@@ -49,3 +55,124 @@ export const Image = styled.img`
     height: 100%;
     display: block;
 `
+export const Form = styled.form`
+    margin-top: 0;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+    padding-bottom: 1rem;
+    width: 30rem;
+`
+
+export const FormGroup = styled.div`
+    &.full{
+        grid-column: 1 / -1;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+        color: #888;
+        font-size: 14px;
+    }
+`
+
+export const FormInput = styled.input`
+    appearance: none;
+    border: 0.5px solid #a9a9a9;
+
+    display: block;
+    width: 80%;
+
+    border-radius: 5px;
+    padding:  12px 16px;
+    background-color: #F3F3F3;
+        :hover{
+            background-color: #acacac;
+            color: white;
+        }
+`
+export const Label = styled.label``
+
+export const Radio = styled.label`
+    display: inline-flex;
+    align-items: center;
+    cursor: pointer;
+`
+
+export const RadioInput = styled.input``
+
+export const RadText = styled.div``
+
+export const Button = styled.button`
+    width: 100%;
+    height: 3.5rem;
+    text-align: center;
+    background-color: #007FFF;
+    border: none;
+    border-radius: 0.2rem;
+    color: white;
+    font-size: 16px;
+    font-weight: 600;
+`
+export const Line = styled.div`
+    height: 0.5px;
+    background: #e5e4e2;
+    margin: 0 2rem;
+`;
+
+export const Div = styled.div``
+
+export const SubTotal = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 3rem;
+`
+export const EstTax = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 3rem;
+    `
+export const PromoCode = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 3rem;
+`
+
+export const SubTText = styled.h4``
+export const EstText = styled.h4``
+export const PromoText = styled.h4`
+    display: inline-flex;
+    align-items: center;
+    margin: 0;
+`
+export const Code = styled.h4`
+    color: #e5e4e2;
+`
+export const Value = styled.h4``
+
+export const Payment = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 3rem;
+    height: 10rem;
+`
+export const Pay = styled.button`
+    width: 12rem;
+    height: 3rem;
+    background: #007FFF;
+    color: white;
+    border: none;
+    font-size: 16px ;
+    font-weight: 500;
+    border-radius: 0.2rem;
+`
+
+export const Total = styled.div`
+    display: flex;
+`
+export const TText = styled.h4``
